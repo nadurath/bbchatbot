0a8ab6382b908d633749c62d00ed2fdd4a1c7b39
sambritton <sambritton212@gmail.com>
2018-11-17 14:20:48 -0600

    added class for getting facts

---
+++ facts.py
@@ -0,0 +1,22 @@
+import re
+
+# This puts the the facts from facts.txt into a dictionary
+def get_facts():
+    f = open('docs/facts.txt')
+    text = f.read()
+    text = text.lower()
+    text = text.replace('--', ' ')
+    text = re.sub(r"[,\/#!?$%\^&\*;:{}=\-\'_`~()\"]+", ' ', text)
+    text = text.split('|')
+    dict = {}
+
+    for x in text:
+        x = x.split('\n')
+        if '' in x:
+            x.remove('')
+        if x[0] not in dict:
+            dict[x[0]] = x[1:len(x)-1]
+
+    return dict
+
+    
