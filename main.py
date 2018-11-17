2f912a085f993e812870082fc997b50aa14fc33a
sambritton <sambritton212@gmail.com>
2018-11-17 17:53:28 -0600

    basic main structure

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 print("Test")
+
+running = True
+introduced = False
+used_facts = {}
+
+'''
+There should also be a handful of current topics such as nouns, subjs, names
+These will be populated and changed by hHR() and used by gBR()
+
+'''
+
+while running:
+    if not introduced:
+        print("AHHHHHH")
+        introduced = True
+
+    #user_response askUser(bot_response)
+
+    #handleHumanResponse(user_response)
+    # this should set the local variables that are interpreted by generateBotResponse
+
+    # bot_response = generateBotResponse(subj,noun,etc....)
+    #This will use the inputs and generate the statement to tell the user
+
+    #promptUser(bot_response)
+
+
+    if True:
+        running = False
+
+    if not running:
+        print("bye {user_name}!")
