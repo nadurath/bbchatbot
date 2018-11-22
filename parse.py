02602ddefd5e0e94504892416ea3fa5efa1d64fd
Dawood Nadurath <dadurath@gmail.com>
2018-11-21 19:54:13 -0600

    updated conversation flow notes

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 
-subjects = ["beach boys","Brian Wilson","Pet Sounds","Mike Love","Good Vibrations","God only knows","Al Jardine","smiley smile","bruce johnston","Carl Wilson","albums","members"]
+subjects = ["albums","members","songs"]
 
-modifiers = ["question","statement","greeting","bot comment","answer","favorite"]
+modifiers = ["statement","greeting","bot comment","answer","favorite"]
 
 # TODO everything
 def handleHumanResponse(text, context):
@@ -14,3 +14,13 @@ def handleHumanResponse(text, context):
     # Basically we just need a bunch of if statements detecting things
 
     return human_response_subjects,human_response_modifiers
+
+''' Convo flow:
+        Greeting - Hello, I'm the Beach Bot! 
+        Branch - Would you like to know about the Beach Boys' members, albums, or songs?) -> State set
+        State (albums, members, songs) - We can tell you about xyz, who/what/which would you like to know more about? -> Pull from facts set
+        State fulfilled - *facts about x*
+        Branch - Would you like to know more about x, some of the (y), or the (z)? -> State set
+        State (albums, members, songs) - 
+        
+'''
