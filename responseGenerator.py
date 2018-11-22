1a32a03bc1f60b82125a35206cc424cdc8cabea0
sambritton <sambritton212@gmail.com>
2018-11-21 23:36:43 -0600

    started on new structure of rG

--- responseGenerator.py
+++ responseGenerator.py
@@ -3,29 +3,33 @@ import factsDictionary
 
 
 def generateFact(memory,facts):
-    generated = "Brian Wilson"
-    memory["topic"] = "brian wilson"
-    if memory["topic"]:
-        if facts[memory["topic"]]:
-            index = random.randint(0, len(facts[memory["topic"]]))
-            generated += " " + facts[memory["topic"]][index]
+    if memory.get("topic"):
+        if facts.get(memory.get("topic")):
+            index = random.randint(0, len(facts[memory["topic"]])-1)
+            generated = memory["topic"] + " " + facts[memory["topic"]][index]
+    return generated
 
-
+def generateContinue(memory):
+    generated =  "Do you want to hear more about "+memory.get("topic")+"?"
     return generated
 
 def generateGoodbye(memory):
-    if memory["name"]:
+    if memory.get("name"):
         audios = "Bye, "+memory["name"]+", thanks for talking!"
         return audios
 
 
 def generate_response(memory):
-    facts = factsDictionary.get_facts()
+    if memory.get("goodbye"):
+        answer = generateGoodbye(memory)
+    else:
+        memory["topic"] = "brian wilson"
+        facts = factsDictionary.get_facts()
+        answer = generateFact(memory,facts)
+        answer += "\n"+generateContinue(memory)
 
-    print(generateFact(memory,facts))
-    answer = "I'm not sure how to respond to that"
-
-
+    if not answer:
+        answer = "I'm not sure how to respond to that"
 
     return answer, memory
 
